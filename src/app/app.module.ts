78fd102f05f13d7668d41703fdb3e5f3e0651710
Lachezar Balev <lachezar.balev@rewe-digital.com>
2018-12-26 09:51:12 +0200

    Initial structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
+import { HomePageComponent } from './home-page/home-page.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomePageComponent,
+    PageNotFoundComponent,
   ],
   imports: [
     BrowserModule

f714cefa221d014d2dfbd97b426c370cb19a6582
Lachezar Balev <lachezar.balev@rewe-digital.com>
2018-12-26 10:58:14 +0200

    Adding a material module, upgrade to Angular 7

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { AppComponent } from './app.component';
 import { HomePageComponent } from './home-page/home-page.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import {AppRoutingModule} from './app-routing.module';
+import { MaterialModule } from './material/material.module';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomePageComponent,
     PageNotFoundComponent,
+    MaterialModule
   ],
   imports: [
     BrowserModule,

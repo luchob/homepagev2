d6c418541a1f36d4634f4126d9543fadc31fdf22
Lachezar Balev <lachezar.balev@gmail.com>
2019-03-14 20:41:53 +0200

    Optimizing the module usage

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,13 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
-import { LayoutModule } from '@angular/cdk/layout';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MainModule } from './main/main.module';
-import {RouterModule} from '@angular/router';
-
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule,
-    LayoutModule,
-    BrowserAnimationsModule,
     MainModule,
     AppRoutingModule
   ],
--- src/app/main/main.module.ts
+++ src/app/main/main.module.ts
@@ -1,18 +1,18 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
 import {FlexLayoutModule} from '@angular/flex-layout';
 import {MaterialModule} from '../material/material.module';
 import {MainNavComponent} from './main-nav/main-nav.component';
 import {PageNotFoundComponent} from './page-not-found/page-not-found.component';
 import {BrowserModule} from '@angular/platform-browser';
 import {RouterModule} from '@angular/router';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [ MainNavComponent, PageNotFoundComponent ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     RouterModule,
-    CommonModule,
     FlexLayoutModule,
     MaterialModule
   ],

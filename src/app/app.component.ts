835da859faa151cefa3d2bd42cc65d2bd46afd50
Lachezar Balev <lachezar.balev@gmail.com>
2019-03-21 21:34:55 +0200

    #6: Add a progress bar when loading the app.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import {Component, OnInit} from '@angular/core';
-import {ActivatedRoute, NavigationEnd, Router} from '@angular/router';
+import {ActivatedRoute, NavigationEnd, NavigationStart, Router} from '@angular/router';
 import {filter, map, mergeMap} from 'rxjs/operators';
 import {SeoService} from './services/seo.service';
 
@@ -10,12 +10,24 @@ import {SeoService} from './services/seo.service';
 })
 export class AppComponent implements OnInit {
 
+  loading = false;
+
   constructor(private router: Router,
               private activatedRoute: ActivatedRoute,
-              private seoService: SeoService) {}
+              private seoService: SeoService) {
+  }
 
   ngOnInit() {
-    this.router.events.pipe(
+
+    const navigationStart$ = this.router.events.pipe(
+      filter((event) => event instanceof NavigationStart)
+    );
+
+    navigationStart$.subscribe( _ => {
+      this.loading = true;
+    });
+
+    const navigationEnd$ = this.router.events.pipe(
       filter((event) => event instanceof NavigationEnd),
       map(_ => this.activatedRoute),
       map((route) => {
@@ -25,18 +37,34 @@ export class AppComponent implements OnInit {
         return route;
       }),
       mergeMap(route => route.data)
-    ).
-    subscribe(event => {
-      const title = event['title'];
-      const desc = event['description'];
+    );
 
-      if (title && desc) {
-        this.seoService.updateTitle(title);
-        this.seoService.updateDescription(desc);
-      }
-
-      window.scrollTo(0, 0);
+    navigationEnd$.subscribe(event => {
+      this.loading = false;
+      this.updateSEO(event);
+      this.scrollToTop();
     });
+  }
+
+  /**
+   * Update the SEO flags.
+   * @param event the content of the navigation event.
+   */
+  private updateSEO(event) {
+    const title = event['title'];
+    const desc = event['description'];
+
+    if (title && desc) {
+      this.seoService.updateTitle(title);
+      this.seoService.updateDescription(desc);
+    }
+  }
+
+  /**
+   * Scrolls the window to top.
+   */
+  private scrollToTop() {
+    window.scrollTo(0, 0);
   }
 
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,14 @@ import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { MainModule } from './main/main.module';
 import {MatProgressBarModule} from '@angular/material';
+import {CommonModule} from '@angular/common';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+    CommonModule,
     MatProgressBarModule,
     MainModule,
     AppRoutingModule
